1581f022315c617ebb12744f9c4a7256041843f7
mithildhure <mithil.dhure1021@gmail.com>
2026-06-30 22:19:47 +0530

    feat(user) : User files

---
+++ src/main/java/course/hub/model/User.java
@@ -0,0 +1,46 @@
+package course.hub.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_details")
+@Getter
+@Setter
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(unique = true, nullable = false)
+	private String username;
+	
+	@Column(unique = true, nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@CreationTimestamp
+	@Column(updatable = false)
+	private LocalDateTime creationTime;
+	
+	@UpdateTimestamp
+	private LocalDateTime updateTime;
+	
+}
---
+++ src/main/java/course/hub/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package course.hub.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import course.hub.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+
+	
+	
+}
