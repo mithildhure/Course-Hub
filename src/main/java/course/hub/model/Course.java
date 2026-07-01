fe46ec29612bbc3476f87cbbcd3628bb04aa6f62
mithildhure <mithil.dhure1021@gmail.com>
2026-07-01 12:36:03 +0530

    feat(user): Course Mapping and Layers added

--- src/main/java/course/hub/model/Course.java
+++ src/main/java/course/hub/model/Course.java
@@ -1,6 +1,7 @@
 package course.hub.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -9,19 +10,23 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Course {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer course_id;
+	private Integer courseId;
 	
 	@Column(nullable = false)
-	private String course_name;
+	private String courseName;
 	
 	@Column(nullable = false)
 	private String description;
@@ -40,6 +45,13 @@ public class Course {
 	@UpdateTimestamp
 	private LocalDateTime courseLastUpdated;
 	
+//	Instructor Mapping
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "instructor_id")
+	private User instructor;
 	
+//	Student Mapping
+	@ManyToMany(mappedBy = "enrolled")
+	private List<User> students;
 	
 }
