3dba9b2d8f9dcc702ed8ebd6cebf9353b4760b66
mithildhure <mithil.dhure1021@gmail.com>
2026-07-01 15:58:49 +0530

    controller

--- src/main/java/course/hub/controller/AuthController.java
+++ src/main/java/course/hub/controller/AuthController.java
@@ -1,10 +1,8 @@
 package course.hub.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
 public class AuthController {
 
 	
