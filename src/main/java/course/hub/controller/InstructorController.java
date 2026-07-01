03a7d1f543cf9386d363ad4b753836a8e8449784
mithildhure <mithil.dhure1021@gmail.com>
2026-07-01 16:51:46 +0530

    Instructor Controller!

---
+++ src/main/java/course/hub/controller/InstructorController.java
@@ -0,0 +1,37 @@
+package course.hub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import course.hub.dto.UserDetailsUpdateDto;
+import course.hub.dto.UserRegisterDto;
+import course.hub.model.User;
+import course.hub.service.InstructorService;
+
+@RestController
+@RequestMapping("/instructor")
+public class InstructorController {
+
+	@Autowired
+	private InstructorService service;
+	
+	@PostMapping("/register")
+	public ResponseEntity<User> registerInstructor(@RequestBody UserRegisterDto dto) {
+		return service.addInstructor(dto); 
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<User> updateInstructorDetails(@RequestParam Integer id, @RequestBody UserDetailsUpdateDto dto){
+		return service.updateInstructorDetails(id, dto);
+	}
+	
+	@PostMapping("/delete")
+	public ResponseEntity<User> deleteInstructor(@RequestParam Integer id){
+		return service.deleteInstructor(id);
+	}	
+}
