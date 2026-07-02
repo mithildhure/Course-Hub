435616e25363a5e776271536541a92f8dbf1269c
mithildhure <mithil.dhure1021@gmail.com>
2026-07-02 17:01:46 +0530

    enrollment logic and transactional for handling rollbacks

--- src/main/java/course/hub/service/StudentService.java
+++ src/main/java/course/hub/service/StudentService.java
@@ -8,13 +8,18 @@ import org.springframework.stereotype.Service;
 import course.hub.dto.UserDetailsUpdateDto;
 import course.hub.model.Course;
 import course.hub.model.User;
+import course.hub.repo.CourseRepo;
 import course.hub.repo.UserRepo;
+import jakarta.transaction.Transactional;
 
 @Service
 public class StudentService {
 
 	@Autowired
 	private UserRepo repo;
+	
+	@Autowired
+	private CourseRepo courseRepo;
 	
 	public List<Course> showStudentEnrolledCourses(Integer id){
 		User user = repo.findById(id).orElseThrow(()-> new RuntimeException("Student Not Found"));
@@ -28,6 +33,20 @@ public class StudentService {
 			user.setUsername(dto.getUsername());
 			user.setPassword(dto.getPassword());
 			return repo.save(user);
+	}
+	
+//	Should have transactional i feel like
+	
+	@Transactional
+	public Course enrollCourse(Integer studentId, Integer courseId) {
+		Course course = courseRepo.findById(courseId).orElseThrow(()-> new RuntimeException("Course not found"));
+		User student = repo.findById(studentId).orElseThrow(()-> new RuntimeException("User Not Found"));
+		if (student.getEnrolled().contains(course)) {
+			throw new RuntimeException("Already Enrolled!");
+		}
+		student.getEnrolled().add(course);
+		repo.save(student);
+		return course;
 	}
 	
 }
