02d1576113634bd330a94746696e5ff58be28005
mithildhure <mithil.dhure1021@gmail.com>
2026-07-04 22:11:46 +0530

    update student thru admin

--- src/main/java/course/hub/service/AdminService.java
+++ src/main/java/course/hub/service/AdminService.java
@@ -1,10 +1,12 @@
 package course.hub.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import course.hub.dto.UserDetailsUpdateDto;
 import course.hub.model.Course;
 import course.hub.model.Role;
 import course.hub.model.User;
@@ -33,6 +35,21 @@ public class AdminService {
 //	Courses
 	public List<Course> getAllCourse(){
 		return courseRepo.findAll();
+	}
+	
+//	Update Student
+	public User updateStudent(Integer id, UserDetailsUpdateDto dto) {
+		Optional<User> opt = userRepo.findById(id);
+		if(opt.isPresent()) {
+			User user = opt.get();
+			user.setName(dto.getName());
+			user.setEmail(dto.getEmail());
+			user.setPassword(dto.getPassword());
+			user.setUsername(dto.getUsername());
+			return userRepo.save(user);
+		}else {
+			throw new RuntimeException("Student not found");
+		}
 	}
 	
 	
